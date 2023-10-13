2b905eb77541a8ebdb71bfa7e60f4d84d992acdc
alexandre21ssouza <alexandre2121ssouza@gmail.com>
2023-10-13 20:07:32 -0300

    Estruturação projeto e configuração de dependencias

---
+++ todolist/src/main/java/com/alexandrebarros/todolist/user/UserModel.java
@@ -0,0 +1,15 @@
+package com.alexandrebarros.todolist.user;
+
+import lombok.Data;
+
+@Data
+public class UserModel {
+
+	private String userName;
+	private String name;
+	private String password;
+	
+	
+		
+	
+}
