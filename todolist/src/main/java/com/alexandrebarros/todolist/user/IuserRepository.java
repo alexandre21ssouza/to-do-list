4a35c59174ca23d8d45d6eb176ecb85179675a9c
alexandre21ssouza <alexandre2121ssouza@gmail.com>
2023-10-14 17:16:48 -0300

    Add da camada model e anotação do banco de dados, criação da tabela

---
+++ todolist/src/main/java/com/alexandrebarros/todolist/user/IuserRepository.java
@@ -0,0 +1,5 @@
+package com.alexandrebarros.todolist.user;
+
+public interface IuserRepository {
+
+}
--- todolist/src/main/java/com/alexandrebarros/todolist/user/UserModel.java
+++ todolist/src/main/java/com/alexandrebarros/todolist/user/UserModel.java
@@ -1,10 +1,22 @@
 package com.alexandrebarros.todolist.user;
 
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
+@Entity(name = "tb_users")
 public class UserModel {
 
+	@Id
+	@GeneratedValue(generator = "UUID")//"UUID" Tipo de Geração de número seguro e único
+	private UUID id;
+	
+	//@Column(name= "usuario") essa anotação define o nome da coluna de "username" p/ "usuario" caso queira personalizar o nome
 	private String userName;
 	private String name;
 	private String password;
