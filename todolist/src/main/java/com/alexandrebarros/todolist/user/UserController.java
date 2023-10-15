d28bd512bb6da146a03d31d9ea9b007f17807d8c
alexandre21ssouza <alexandre2121ssouza@gmail.com>
2023-10-15 15:19:33 -0300

    add classe TaskModel, repository, controller

--- todolist/src/main/java/com/alexandrebarros/todolist/user/UserController.java
+++ todolist/src/main/java/com/alexandrebarros/todolist/user/UserController.java
@@ -1,18 +1,41 @@
 package com.alexandrebarros.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+
 @RestController
 @RequestMapping("/users")
 public class UserController {
+	
+	@Autowired
+	private IUserRepository userRepository;
 
 	@PostMapping("/")
-	public void create(@RequestBody UserModel userModel) {
+	public ResponseEntity create(@RequestBody UserModel userModel) {
+		var user = this.userRepository.findByUserName(userModel.getUserName());
+	
+		if(user != null) {
+			System.out.println("Usuário já existe");
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
+		}
 		
-		System.out.println(userModel.getName());
+		//Faz a cryptografia da senha
+		var passwordHashred = BCrypt.withDefaults()
+				.hashToString(12,userModel.getPassword().toCharArray());
+		
+		userModel.setPassword(passwordHashred);
+		
+		var userCreated = this.userRepository.save(userModel);
+		return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
+		
 	}
 	
 }
--- todolist/src/main/java/com/alexandrebarros/todolist/user/UserModel.java
+++ todolist/src/main/java/com/alexandrebarros/todolist/user/UserModel.java
@@ -1,6 +1,11 @@
 package com.alexandrebarros.todolist.user;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
+
+import javax.print.attribute.standard.DateTimeAtCreation;
+
+import org.hibernate.annotations.CreationTimestamp;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -17,9 +22,13 @@ public class UserModel {
 	private UUID id;
 	
 	//@Column(name= "usuario") essa anotação define o nome da coluna de "username" p/ "usuario" caso queira personalizar o nome
+	@Column(unique = true)// Determina que os dados sejam únicos no banco(constrain)
 	private String userName;
 	private String name;
 	private String password;
+	
+	@CreationTimestamp //Essa anotação salva a data da criação do User
+	private LocalDateTime createdAt;
 	
 	
 		
